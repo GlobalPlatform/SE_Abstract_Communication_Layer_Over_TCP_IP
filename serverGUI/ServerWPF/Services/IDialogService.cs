f95b4edc295dfe81a583f66642e20036c3a5e76f
ChristopherCastel <castelchristopher95@gmail.com>
2019-04-09 15:27:56 +0200

    Quick visual fix

--- serverGUI/ServerWPF/Services/IDialogService.cs
+++ serverGUI/ServerWPF/Services/IDialogService.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ServerWPF.Services
+﻿namespace ServerWPF.Services
 {
     public interface IDialogService
     {
